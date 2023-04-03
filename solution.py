04c2110e736a4db6172a0f97b086c915282a23b7
ash0tych <60326128+ash0tych@users.noreply.github.com>
2023-04-03 23:38:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,13 @@ import numpy as np
 chat_id = 690310158 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() / 21 # Ваш ответ
+    import numpy as np
+    from scipy.optimize import minimize
+    
+    def objective(a: float, x: np.array) -> float:
+    t = 21
+    s_estimated = a * (t**2) / 2
+    return np.sum(np.abs(s_estimated - x))
+    
+    result = minimize(objective, 0, args=(x), method="L-BFGS-B")
+    return result.x[0]
