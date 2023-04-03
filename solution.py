0500cb5642c87cacbc3798d02cb90681d664c2b9
ash0tych <60326128+ash0tych@users.noreply.github.com>
2023-04-03 23:27:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = chat_id # Ваш chat ID, не меняйте название переменной
+chat_id = 690310158 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
